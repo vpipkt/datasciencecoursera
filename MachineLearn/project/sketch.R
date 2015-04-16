b85327d4478c387ac44350e101febb47f6be9d77
Jason Brown <brown27@gmail.com>
2015-04-15 21:01:16 -0400

    Initial project exploration

---
+++ MachineLearn/project/sketch.R
@@ -0,0 +1,22 @@
+trainUrl <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
+trainDir <- "data/pml-training.csv"
+testUrl <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+testDir <- "data/pml-testing.csv"
+
+download.file(trainUrl, trainDir)
+download.file(testUrl, testDir)
+
+#need to set "#DIV/O!" as a NA
+training <- read.csv(trainDir, na.strings = c("NA","#DIV/0!"))
+names(training)
+
+#first field looks like a row name
+summary(training$X - 1:nrow(training))
+
+#not sure what these are
+summary(training$new_window)
+summary(training$num_window)
+
+summary(training[,20:40])
+classes(training)
+str(training)
